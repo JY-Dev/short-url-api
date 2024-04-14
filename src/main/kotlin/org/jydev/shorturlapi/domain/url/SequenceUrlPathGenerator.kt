d7bba9725acc8e08304f52ff20869ffd79ecaada
jaeyoung <kjy0302014@gmail.com>
2024-04-14 19:42:38 +0900

    [feat] SequenceUrlPathGenerator 생성

---
+++ src/main/kotlin/org/jydev/shorturlapi/domain/url/SequenceUrlPathGenerator.kt
@@ -0,0 +1,25 @@
+package org.jydev.shorturlapi.domain.url
+
+class SequenceUrlPathGenerator {
+
+    private val chars = ('a'..'z') + ('A'..'Z')
+
+    fun generate(idx : Long, urlLength : Int) : String {
+
+        val base = chars.size
+        var currentIdx = idx
+        val result = StringBuilder()
+
+        for (i in 0 until urlLength) {
+            result.insert(0, chars[(currentIdx % base).toInt()])
+            currentIdx /= base
+        }
+
+        if (currentIdx > 0) {
+            throw IllegalArgumentException("Index exceeds the number of possible words for the given length")
+        }
+
+        return result.toString()
+    }
+
+}
---
+++ src/test/kotlin/org/jydev/shorturlapi/domain/url/SequenceUrlPathGeneratorTest.kt
@@ -0,0 +1,39 @@
+package org.jydev.shorturlapi.domain.url
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
+
+class SequenceUrlPathGeneratorTest {
+
+    private val generator = SequenceUrlPathGenerator()
+
+    @ParameterizedTest
+    @CsvSource(
+        "0, 1, a",
+        "1, 1, b",
+        "0, 2, aa",
+        "1, 2, ab",
+        "52, 2, ba",
+        "2703, 2, ZZ"
+    )
+    fun `idx와 length가 주어지면 URL PATH는 length 길이와 idx 순서에 맞게 생성 되어야 한다`(
+        idx: Long, urlLength: Int, expectedOutput: String
+    ) {
+        Assertions.assertEquals(
+            expectedOutput,
+            generator.generate(idx, urlLength),
+            "Failed at idx $idx with length $urlLength"
+        )
+    }
+
+    @Test
+    fun `length로 만들 수 있는 단어의 갯수보다 idx가 더 큰 경우 예외가 발생 해야한다 `() {
+        val largeIndex = 52L * 52L + 1
+
+        Assertions.assertThrows(IllegalArgumentException::class.java, {
+            generator.generate(largeIndex, 2)
+        }, "Should throw IllegalArgumentException for index that exceeds possible words")
+    }
+}
